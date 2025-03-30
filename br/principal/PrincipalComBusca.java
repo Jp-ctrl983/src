2dd53465017d979c29b28577af82afaf7711d268
João Pedro <joao.fernandes37.pedro@gmail.com>
2025-03-30 20:51:08 -0300

    Atualizei

--- br/principal/PrincipalComBusca.java
+++ br/principal/PrincipalComBusca.java
@@ -1,4 +1,14 @@
 package br.principal;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+
 public class PrincipalComBusca {
+    public static void main(String[] args) {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://www.omdbapi.com/?t=matrix&apikey=2b316357"))
+                .build();
+    }
 }
