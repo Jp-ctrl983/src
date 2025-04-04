59361d2c1c1c6561ccdb2621aa8ab4554023ebc8
João Pedro <joao.fernandes37.pedro@gmail.com>
2025-04-03 21:40:53 -0300

    Update

--- br/principal/PrincipalComBusca.java
+++ br/principal/PrincipalComBusca.java
@@ -5,18 +5,27 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class PrincipalComBusca {
     public static void main(String[] args) throws IOException, InterruptedException {
 
+        Scanner  scan = new Scanner(System.in);
+        System.out.println("O que vc deseja assistir: ");
+        String busca = scan.nextLine();
+
+        String endereco = "https://www.omdbapi.com/?t=" + busca + "&apikey=2b316357";
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://www.omdbapi.com/?t=matrix&apikey=2b316357"))
+                .uri(URI.create(endereco))
                 .build();
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
 
-        System.out.println(response.body());
+        System.out.println(response.body() + "\n\n");
+
+        System.out.println("Se der erro verifique se vc não digitou nada de errado");
 
     }
 }
