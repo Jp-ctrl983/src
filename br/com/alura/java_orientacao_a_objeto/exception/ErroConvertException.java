84a18b8aceb441ce7c3484e23f6757fa8956605f
João Pedro <joao.fernandes37.pedro@gmail.com>
2025-07-01 11:30:32 -0300

    Update

--- br/com/alura/java_orientacao_a_objeto/exception/ErroConvertException.java
+++ br/com/alura/java_orientacao_a_objeto/exception/ErroConvertException.java
@@ -1,7 +1,6 @@
 package br.com.alura.java_orientacao_a_objeto.exception;
 
 public class ErroConvertException extends RuntimeException {
-
     private String mensagem;
 
     public ErroConvertException(String msg) {
